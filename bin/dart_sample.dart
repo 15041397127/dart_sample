a68779fb31e053b7268260b5e39fe95588a55ba9
zhangxu <zhangxusy@foxmail.com>
2019-12-31 14:29:48 +0800

    增加代码 验证travis

--- bin/dart_sample.dart
+++ bin/dart_sample.dart
@@ -12,3 +12,14 @@ class Meta{
   //成员变量初始化语法糖
   Meta(this.price,this.name);
 }
+
+class Item extends Meta{
+
+   
+  Item(name,price):super(name,price);
+
+
+  //重载+运算符 将商品对象合并为套餐商品
+  Item operator+(Item item) => Item(name + item.name,price+item.price);
+
+}
